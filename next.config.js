6f26a7b002a77262c4efad9feaa903c682026e69
Jpestel <jerompestel@gmail.com>
2026-05-19 11:32:49 +0200

    Fix: alias webpack explicite pour résoudre @/ sur Node 21

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -5,6 +7,13 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+    }
+    return config
   },
 }
 module.exports = nextConfig
